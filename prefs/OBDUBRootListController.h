cf76eb3e565918780039afbe65355a44bbb0ceb8
551 <wdf@hotmail.co.uk>
2026-09-13 11:38:47 +0100

    Add reset controls for both OBDeleven apps

--- prefs/OBDUBRootListController.h
+++ prefs/OBDUBRootListController.h
@@ -5,7 +5,8 @@
 @interface OBDUBRootListController : PSListController
 - (id)readPreferenceValue:(PSSpecifier *)specifier;
 - (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier;
-- (void)resetSpoofDefaults;
+- (void)resetOBDDefaults;
+- (void)resetVAGDefaults;
 - (void)respring;
 - (void)openGitHub;
 @end
