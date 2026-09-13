a9339c36ba8128d1f3c1c056efd3218fa27d93b9
551 <wdf@hotmail.co.uk>
2026-09-13 09:34:12 +0100

    Spoof VAG 1.9.28 network identity as current client

--- Tweak.m
+++ Tweak.m
@@ -7,6 +7,8 @@
 static NSString *const kTargetBundle = @"com.voltasit.obdeleven.ios";
 static NSString *const kTargetVersion = @"1.9.28";
 static NSString *const kTargetBuild = @"1704712364";
+static NSString *const kServerVersion = @"1.9.73";
+static NSString *const kServerBuild = @"1785335496";
 static NSString *const kPreferencesPath = @"/var/mobile/Library/Preferences/com.551.obdelevenupdatebypass.plist";
 
 // OBDeleven VAG 1.9.28: final result of UpdateUtility.isForceUpdateNeeded(completion:).
@@ -19,6 +21,85 @@ static BOOL tweakEnabled(void) {
     NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:kPreferencesPath];
     id value = prefs[@"vagEnabled"];
     return value ? [value boolValue] : YES;
+}
+
+static NSURLRequest *requestBySpoofingServerIdentity(NSURLRequest *request) {
+    if (!request) return request;
+
+    NSMutableURLRequest *mutable = [request mutableCopy];
+    [mutable setValue:kServerVersion forHTTPHeaderField:@"x-mobile-app-version"];
+    [mutable setValue:kServerBuild forHTTPHeaderField:@"x-mobile-app-build"];
+
+    // Older VAG builds also expose their app version/build through the User-Agent.
+    // Rewrite only those two known values and leave device/OS identity unchanged.
+    NSString *userAgent = [mutable valueForHTTPHeaderField:@"User-Agent"];
+    if (userAgent.length > 0) {
+        NSString *spoofed = [userAgent stringByReplacingOccurrencesOfString:kTargetVersion
+                                                                 withString:kServerVersion];
+        spoofed = [spoofed stringByReplacingOccurrencesOfString:kTargetBuild
+                                                     withString:kServerBuild];
+        [mutable setValue:spoofed forHTTPHeaderField:@"User-Agent"];
+    }
+
+    return mutable;
+}
+
+typedef NSURLSessionDataTask *(*DataTaskRequestCompletionIMP)(
+    NSURLSession *, SEL, NSURLRequest *,
+    void (^)(NSData *, NSURLResponse *, NSError *));
+static DataTaskRequestCompletionIMP originalDataTaskRequestCompletion = NULL;
+
+static NSURLSessionDataTask *spoofedDataTaskRequestCompletion(
+    NSURLSession *self,
+    SEL _cmd,
+    NSURLRequest *request,
+    void (^completion)(NSData *, NSURLResponse *, NSError *)) {
+    return originalDataTaskRequestCompletion(
+        self, _cmd, requestBySpoofingServerIdentity(request), completion);
+}
+
+typedef NSURLSessionDataTask *(*DataTaskRequestIMP)(NSURLSession *, SEL, NSURLRequest *);
+static DataTaskRequestIMP originalDataTaskRequest = NULL;
+
+static NSURLSessionDataTask *spoofedDataTaskRequest(
+    NSURLSession *self,
+    SEL _cmd,
+    NSURLRequest *request) {
+    return originalDataTaskRequest(self, _cmd, requestBySpoofingServerIdentity(request));
+}
+
+typedef NSURLSessionUploadTask *(*UploadTaskDataCompletionIMP)(
+    NSURLSession *, SEL, NSURLRequest *, NSData *,
+    void (^)(NSData *, NSURLResponse *, NSError *));
+static UploadTaskDataCompletionIMP originalUploadTaskDataCompletion = NULL;
+
+static NSURLSessionUploadTask *spoofedUploadTaskDataCompletion(
+    NSURLSession *self,
+    SEL _cmd,
+    NSURLRequest *request,
+    NSData *bodyData,
+    void (^completion)(NSData *, NSURLResponse *, NSError *)) {
+    return originalUploadTaskDataCompletion(
+        self, _cmd, requestBySpoofingServerIdentity(request), bodyData, completion);
+}
+
+static void installNetworkSpoofs(void) {
+    Class sessionClass = [NSURLSession class];
+
+    MSHookMessageEx(sessionClass,
+                    @selector(dataTaskWithRequest:completionHandler:),
+                    (IMP)spoofedDataTaskRequestCompletion,
+                    (IMP *)&originalDataTaskRequestCompletion);
+
+    MSHookMessageEx(sessionClass,
+                    @selector(dataTaskWithRequest:),
+                    (IMP)spoofedDataTaskRequest,
+                    (IMP *)&originalDataTaskRequest);
+
+    MSHookMessageEx(sessionClass,
+                    @selector(uploadTaskWithRequest:fromData:completionHandler:),
+                    (IMP)spoofedUploadTaskDataCompletion,
+                    (IMP *)&originalUploadTaskDataCompletion);
 }
 
 __attribute__((constructor))
@@ -45,6 +126,9 @@ static void Init(void) {
         }
 
         MSHookMemory(target, kNoUpdateInstruction, sizeof(kNoUpdateInstruction));
-        NSLog(@"[OBD11VAG-iOS14] 1.9.28 force-update check patched");
+        installNetworkSpoofs();
+
+        NSLog(@"[OBD11VAG-iOS14] 1.9.28 patched; server identity %@ (%@)",
+              kServerVersion, kServerBuild);
     }
 }
